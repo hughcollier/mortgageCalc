3cd5e5abb5b6ed01672b16fbe21cb8fcbbd68ca1
Hugh Collier <hughcollier@Hughs-iMac.local>
2022-11-03 23:01:47 +0000

    Added basic light / dark theme switching using localStorage

--- src/modules/theme.js
+++ src/modules/theme.js
@@ -1,25 +1,43 @@
 function switchTheme() {
-
+	const bodyElement = document.querySelector("body");	
 	const themeToggle = document.getElementById("switch-theme-button");
-	const bodyElement = document.querySelector("body");							
+
+	const defaultTheme = "light";
+	let currentTheme
+
+	if (localStorage.getItem("theme") === null) {
+		// Nothing in local storage so use default theme
+		currentTheme = defaultTheme;
+
+	} else {
+		// Theme is being set by value in local storage
+		currentTheme = localStorage.getItem("theme");
+	};
 	
+	if (currentTheme === "light") {
+		bodyElement.classList.add("light-theme");
+	};
+							
+	themeToggle.innerHTML = currentTheme === "dark" ? "Activate Light Theme" : "Activate Dark Theme";	
+
 	themeToggle.addEventListener("click", () => {
-			
-		if (themeToggle.innerHTML = "Activate Light Theme") {
+	
+		if (currentTheme === "dark") {
 			themeToggle.innerHTML = "Activate Dark Theme";
-			console.log("test");
-
-		} else if (themeToggle.innerHTML = "Activate Dark Theme") {
+			currentTheme = "light";
+			
+		} else if (currentTheme = "light") {
 			themeToggle.innerHTML = "Activate Light Theme";
-
+			currentTheme = "dark";
 		};
 
 		bodyElement.classList.toggle("light-theme");
+		localStorage.setItem("theme", currentTheme);
 
-
+		console.log(currentTheme);
+	
 	})
 	
-
 };
 
 export {switchTheme,};
