0c44b9026a01f82104ad44d91aaa422451c0615b
Hugh Collier <collier.289@gmail.com>
2022-12-12 17:00:19 +0000

    Updated theme switching setting from toggle to individual buttons

--- src/modules/settings.js
+++ src/modules/settings.js
@@ -15,6 +15,18 @@ const settings = {
 		return themeButtonText;
 	},	
 	
+	activateDarkTheme: function() {
+		this.theme = "dark";
+		localStorage.setItem("theme", this.theme);
+	},
+	
+	activateLightTheme: function() {
+		this.theme = "light";
+		localStorage.setItem("theme", this.theme);
+	},
+	
+	
+	
 	swapTableDisplay: function() {
 		removePreviousResults();
 		this.displayTable = this.displayTable === "month" ? this.displayTable = "year" : "month";
@@ -51,15 +63,15 @@ function switchCurrency() {
 	currencySwitchButtons.forEach((button) => {
 
 		if (button.getAttribute("data-currency") === settings.currency) {
-			button.classList.add("active-currency");				
+			button.classList.add("active");				
 		}
 
 		button.addEventListener("click", () => {
 			for (let i = 0; i < currencySwitchButtons.length; i++ ) {
-				currencySwitchButtons[i].classList.remove("active-currency");
+				currencySwitchButtons[i].classList.remove("active");
 			}
 			let currency = button.getAttribute("data-currency");
-			button.classList.add("active-currency");
+			button.classList.add("active");
 			settings.setCurrency(currency);			
 			
 			// update display with new currency by removing previous results and simulating click on the "calculate" button
--- src/modules/theme.js
+++ src/modules/theme.js
@@ -2,20 +2,44 @@ import {settings} from "./settings.js";
 
 function switchTheme() {
 	const bodyElement = document.querySelector("body");	
-	const themeToggle = document.getElementById("switch-theme-button");
+	const darkThemeButton = document.getElementById("activate-dark-theme");
+	const lightThemeButton = document.getElementById("activate-light-theme");
 
 	if (localStorage.getItem("theme") !== null) { settings.theme = window.localStorage.getItem('theme'); };
-	if (settings.theme === "light") { bodyElement.classList.add("light-theme"); };							
+	
+	if (settings.theme === "light") { 
+		bodyElement.classList.add("light-theme"); 
+		lightThemeButton.classList.add("active");
+	};							
+	
+	if (settings.theme === "dark") {
+		darkThemeButton.classList.add("active");
+	};
+		
+	lightThemeButton.addEventListener("click", () => {
+		settings.activateLightTheme();
+		
+		bodyElement.classList.add("light-theme");
+		lightThemeButton.classList.add("active");
+		
+		bodyElement.classList.remove("dark-theme",);
+		darkThemeButton.classList.remove("active");
+		
+	});
+	
+	darkThemeButton.addEventListener("click", () => {
+		settings.activateDarkTheme();
+		
+		bodyElement.classList.add("dark-theme");
+		darkThemeButton.classList.add("active");
+		
+		bodyElement.classList.remove("light-theme",);
+		lightThemeButton.classList.remove("active");
+		
+	});
 	
 	
-	let themeButtonText = settings.theme === "dark" ? "Activate Light Theme" : "Activate Dark Theme";	
-	themeToggle.innerHTML = themeButtonText;
-
-	themeToggle.addEventListener("click", () => {
-			themeButtonText = settings.swapTheme();
-			themeToggle.innerHTML = themeButtonText;
-			bodyElement.classList.toggle("light-theme");
-	})
+	
 };
 
 export {switchTheme,};
